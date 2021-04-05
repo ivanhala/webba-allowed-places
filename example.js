a7145cc72806ce98cfc1d516ed36fb062bd21a48
Ivan <ivan@plugion.org>
2021-04-05 10:18:42 +0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,8 @@
+function wbk_on_form_rendered( service ){
+    jQuery('#wbk-book-quantity').find('option').remove();
+    var options = '';
+    for( var i = 1; i <= 10; i++ ){
+      options += '<option value='+ i  + '>' + i + '</option>';
+    }
+	  jQuery('#wbk-book-quantity').html(options);
+}
